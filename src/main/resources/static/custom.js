7797c95ea3144a27f066390f9737f945adc16131
Jenny Tang <jennydtang@gmail.com>
2020-08-04 16:07:08 -0400

    add styling, markers, info window

--- src/main/resources/static/custom.js
+++ src/main/resources/static/custom.js
@@ -8,6 +8,13 @@ function initMap() {
     zoom: 15,
     scrollwheel: false,
   });
+  var contentString = "<h1>Bus #</h1>" + "<h2>" + busLocations + "</h2>";
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString,
+  });
+
+  // Add Bus marker
+  var image = { url: "busicon.png", scaledSize: new google.maps.Size(50, 50) };
 
   for (i = 0; i < busLocations.length; i++) {
     var marker = new google.maps.Marker({
@@ -16,6 +23,10 @@ function initMap() {
         lng: parseFloat(busLocations[i].LONGITUDE),
       },
       map: map,
+      icon: image,
+    });
+    marker.addListener("click", () => {
+      infowindow.open(map, marker);
     });
   }
 }
